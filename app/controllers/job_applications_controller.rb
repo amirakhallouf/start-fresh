ace318ffbc2f82fa4b0580085a80bdfb34c99c6d
amira khallouf <naseam1@hotmail.com>
2017-08-23 18:06:47 +0100

    lots of things for setup

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,5 +1,31 @@
 class JobApplicationsController < ApplicationController
+
   def index
     @job_applications = JobApplication.all
   end
+
+  def new
+    @job_application = JobApplication.new
+    @job_offer = JobOffer.find(params[:job_offer_id])
+  end
+
+  def create
+
+    @job_application = JobApplication.new(job_application_params)
+    @job_application.user = current_user
+    @job_application.status = "Pending"
+    @job_application.job_offer = JobOffer.find(params[:job_offer_id])
+
+    if @job_application.save
+      redirect_to job_offer_path(@job_application.job_offer)
+    end
+
+  end
+
+  private
+
+  def job_application_params
+    params.require(:job_application).permit(:personal_motivation)
+  end
+
 end
